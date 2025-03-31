71432ace1823239f85800a06f50bfb65c5f5cea6
Sujal7ss <sujalchahande3@gmail.com>
2025-03-31 12:22:02 +0530

    Project Initiated with Vite + Tailwind + Shadcn

---
+++ mock-interview/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button variant={"default"} size={"sm"}>Click me</Button>
+    </>
+  )
+}
+
+export default App
