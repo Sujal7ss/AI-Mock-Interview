1e5f9bc7e9124fe3e339b1ffc5cccc0801ed3dd8
Sujal7ss <sujalchahande3@gmail.com>
2025-04-01 19:08:58 +0530

    clerk auth and routing setup

--- mock-interview/src/App.tsx
+++ mock-interview/src/App.tsx
@@ -1,34 +1,40 @@
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom"
-import PublicLayout from "@/layouts/public-layout"
-import Home from "@/routes/home"
-import AuthLayout from "@/layouts/auth-layout"
-import SignInPage from "@/routes/sign-in"
-import SingUpPage from "@/routes/sign-up"
-import { ProtectedLayout } from "./layouts/protected-layout"
-import MainLayout from "./layouts/main-layout"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicLayout from "@/layouts/public-layout";
+import Home from "@/routes/home";
+import AuthLayout from "@/layouts/auth-layout";
+import SignInPage from "@/routes/sign-in";
+import SingUpPage from "@/routes/sign-up";
+import { ProtectedLayout } from "./layouts/protected-layout";
+import MainLayout from "./layouts/main-layout";
 function App() {
-
   return (
     <>
-    <Router>
-      <Routes>
-        {/* Public routes */}
-        <Route element={<PublicLayout />}>
-          <Route index element={<Home />} />
-        </Route>
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route element={<PublicLayout />}>
+            <Route index element={<Home />} />
+          </Route>
 
-        {/* auth routes */}
-        <Route element={<AuthLayout />}>
-          <Route path="/signin/*" element={<SignInPage />} />
-          <Route path="/signup/*" element={<SingUpPage />} />
-        </Route>
-        {/* Protected Routes */}
-        <Route element={<ProtectedLayout > <MainLayout /></ProtectedLayout>}>
-        </Route>
-      </Routes>
-    </Router>
+          {/* auth routes */}
+          <Route element={<AuthLayout />}>
+            {/* <Route path="/signin/*" element={<SignInPage />} /> */}
+            <Route path="/signup/*" element={<SingUpPage />} />
+          </Route>
+          {/* Protected Routes */}
+          <Route
+            element={
+              <ProtectedLayout>
+                {" "}
+                <MainLayout />
+              </ProtectedLayout>
+            }
+          >
+          </Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- mock-interview/src/layouts/main-layout.tsx
+++ mock-interview/src/layouts/main-layout.tsx
@@ -1,15 +1,20 @@
-import Footer from "@/components/footer"
-import Header from "@/components/header"
-import { Outlet } from "react-router-dom"
+import { Container } from "@/components/container";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
     <div className="flex flex-col h-screen">
-        <Header/>
-        <Outlet/>
-        <Footer/>
+      <Header />
+      <Container className="flex-grow">
+        <main className="flex-grow">
+        <Outlet />
+        </main>
+      </Container>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
